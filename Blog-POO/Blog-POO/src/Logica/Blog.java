3f2ecd24aca34cf5b0fc5d870a7792609773383e
KZ14-debug <kendallzamora2@gmail.com>
2026-04-22 18:32:14 -0600

    Add files via upload

---
+++ Blog-POO/Blog-POO/src/Logica/Blog.java
@@ -0,0 +1,121 @@
+package Logica;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Blog 
+{
+	
+	private String nombre;
+	private int codigo;
+	private String descripcion;
+	private LocalDateTime fechaDeCreacion;
+	private int codigoNumerico;
+	private Map<Integer, Publicacion> publicaciones;
+	
+	
+	
+	public Blog(String nombre, String descripcion)
+	{
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		codigo = codigoNumerico;
+		codigoNumerico++;
+		fechaDeCreacion = LocalDateTime.now();
+		publicaciones = new TreeMap<Integer, Publicacion>();
+		
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+	public LocalDateTime getFechaDeCreacion() {
+		return fechaDeCreacion;
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void crearPublicacion(String titulo, String texto, String autor)
+	{
+		Publicacion publi = new Publicacion(titulo, texto, autor);
+		publicaciones.put(publi.getCodigoP(), publi);
+		
+	}
+	
+	
+	private void revisarPublicacionExistente(int codigoP) throws Exception
+	{
+		if(!publicaciones.containsKey(codigoP))
+		{
+			throw new Exception("Codigo de publicacion no encontrado.");
+		}
+	}
+
+	public String obtenerPublicacionyComentarios(int codigoP) throws Exception
+	{
+		revisarPublicacionExistente(codigoP);
+		
+		if(!publicaciones.containsKey(codigoP))
+		{
+			throw new Exception("Codigo de publicacion no encontrado");
+		}
+		
+		Publicacion publi = publicaciones.get(codigoP);
+		return publi.toString();
+	}
+	
+	
+	public Map<Integer, String> coleccionTitulosPublicaciones()
+	{
+		Map<Integer,String> titulos = new TreeMap<Integer, String>();
+		
+		for(Publicacion publi : publicaciones.values())
+		{
+			titulos.put(publi.getCodigoP(), publi.getTitulo());
+		}
+		
+		return titulos;
+	}
+	
+	public void agregarComentarioenPublicacion(int codigoP, String emailAutor, String direccionIP, String texto) throws Exception
+	{
+	
+		
+		revisarPublicacionExistente(codigoP);
+		Publicacion publi = publicaciones.get(codigoP);
+		publi.agregarComentario(emailAutor, direccionIP, texto);
+		
+	}
+	
+	
+	public void borrarComentarioenPublicacion(int codigoP, int posicion) throws Exception
+	{
+		
+		
+		revisarPublicacionExistente(codigoP);
+		Publicacion publi = publicaciones.get(codigoP);
+		publi.borrarComentario(posicion);
+	}
+	
+}
---
+++ Blog-POO/Blog-POO/src/Logica/Comentario.java
@@ -0,0 +1,55 @@
+package Logica;
+
+import java.time.LocalDateTime;
+
+public class Comentario {
+	
+	private String emailAutor;
+	private String texto;
+	private LocalDateTime fecha;
+	private String direccionIP;
+	
+	public Comentario(String emailAutor, String texto, String direccionIP)
+	{
+		this.emailAutor = emailAutor;
+		this.texto = texto;
+		this.direccionIP = direccionIP;
+		fecha = LocalDateTime.now();
+		
+	}
+
+	public String getEmailAutor() {
+		return emailAutor;
+	}
+
+	public void setEmailAutor(String emailAutor) {
+		this.emailAutor = emailAutor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public String getDireccionIP() {
+		return direccionIP;
+	}
+	
+	public String toString()
+	{
+		String resultado= "Email: " + emailAutor + " - "
+	            + "Fecha de publicacion: " + fecha.toString() + "\n"
+	            + "Texto: " + texto + "\n";
+		
+		return resultado;
+	}
+	
+
+}
